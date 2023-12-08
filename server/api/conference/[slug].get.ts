6479bc6d1ea283469e78ccd1b5d0a3f71d6c4d95
Daniel Roe <daniel@roe.dev>
2023-12-08 12:49:59 +0100

    feat: add JSON schema for conferences

--- server/api/conference/[slug].get.ts
+++ server/api/conference/[slug].get.ts
@@ -1,7 +1,7 @@
 export default defineEventHandler(async event => {
   const storage = useStorage('conferences')
   const slug = getRouterParam(event, 'slug')
-  if (!slug || !(await storage.hasItem(slug + '.json'))) {
+  if (!slug || slug.startsWith('_') || !(await storage.hasItem(slug + '.json'))) {
     throw createError({ statusCode: 404 })
   }
   return storage.getItem(slug + '.json')
--- server/api/conferences.get.ts
+++ server/api/conferences.get.ts
@@ -1,5 +1,5 @@
 export default defineEventHandler(async event => {
   const storage = useStorage('conferences')
-  const conferences = await storage.getKeys()
+  const conferences = await storage.getKeys().then(keys => keys.filter(key => !key.startsWith('_')))
   return Promise.all(conferences.map(async key => storage.getItem(key)))
 })
