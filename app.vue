3375b6f0ca340241e2dc8d0827aebd15da9b3efe
beerose <alexsandra.sikora@gmail.com>
2023-12-11 23:12:16 +0100

    update favicon

--- app.vue
+++ app.vue
@@ -1,4 +1,10 @@
 <template>
+  <Head>
+    <Link
+      rel="icon"
+      href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🫠</text></svg>"
+    />
+  </Head>
   <NuxtLayout>
     <Hero />
     <ConfList />
@@ -12,4 +18,10 @@
   import Footer from '~/components/Footer.vue'
   import CTA from '~/components/CTA.vue'
   import ConfList from '~/components/ConfList.vue'
+
+  useSeoMeta({
+    title: 'diversity.lol',
+    description:
+      'A list of conferences and events with statistics on diversity and inclusion.',
+  })
 </script>
