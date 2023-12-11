2d31f02c07624e7b8bc8611be86d339680b1ae47
beerose <alexsandra.sikora@gmail.com>
2023-12-11 21:14:50 +0100

    Remove padidng from footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer>
     <div
-      class="mx-auto max-w-7xl px-6 py-12 md:flex md:items-center md:justify-between lg:px-8"
+      class="mx-auto max-w-7xl py-12 md:flex md:items-center md:justify-between"
     >
       <div class="flex justify-center space-x-6 md:order-2">
         <a
