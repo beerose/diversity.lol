9308e4e76d1acd6921843a5b342b6ae5098ee813
beerose <alexsandra.sikora@gmail.com>
2023-12-11 21:14:35 +0100

    Update layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-    <div class="mx-auto max-w-4xl">
+  <div class="mx-auto max-w-6xl px-4 sm:px-6 lg:px-8">
+    <div class="mx-auto max-w-3xl">
       <slot />
     </div>
   </div>
