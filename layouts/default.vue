7e87c0538b715d8c644b8c22fcce299a979dbb3c
Michal Miszczyszyn <michal@mmiszy.pl>
2024-01-25 13:22:54 +0100

    Initial commit

--- layouts/default.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="mx-auto max-w-6xl px-4 sm:px-6 lg:px-8">
-    <div class="mx-auto max-w-3xl">
-      <slot />
-    </div>
-  </div>
-</template>
--- server/api/conference/[slug].get.ts
+++
@@ -1,8 +0,0 @@
-export default defineEventHandler(async event => {
-  const storage = useStorage('conferences')
-  const slug = getRouterParam(event, 'slug')
-  if (!slug || slug.startsWith('_') || !(await storage.hasItem(slug + '.json'))) {
-    throw createError({ statusCode: 404 })
-  }
-  return storage.getItem(slug + '.json')
-})
