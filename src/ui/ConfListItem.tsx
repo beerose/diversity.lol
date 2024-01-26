1e11ff7bbb2e89d260379d0cb214a728758d507b
beerose <alexsandra.sikora@gmail.com>
2024-01-26 12:52:41 +0000

    Add CFP open status info

--- src/ui/ConfListItem.tsx
+++ src/ui/ConfListItem.tsx
@@ -2,9 +2,12 @@ import Link from "next/link";
 import { type Conference } from "@/data/types/v1";
 
 export const ConfListItem = ({ conference }: { conference: Conference }) => {
+	const isCFPOpen =
+		new Date(conference.cfp?.open ?? "") < new Date() && new Date(conference.cfp?.close ?? "") > new Date();
+
 	return (
-		<li className="relative flex gap-x-6 rounded-md border border-slate-100">
-			<div className="flex items-center justify-center bg-primary px-2 text-sm">
+		<li className="relative grid grid-cols-5 gap-x-6 rounded-md border border-slate-100">
+			<div className="col-span-1 flex items-center justify-center bg-primary px-2 text-sm">
 				<time dateTime={conference.startDate} className="px-4 text-center font-semibold text-slate-800">
 					{new Date(conference.startDate).toLocaleDateString("en-US", {
 						month: "short",
@@ -13,7 +16,7 @@ export const ConfListItem = ({ conference }: { conference: Conference }) => {
 					})}
 				</time>
 			</div>
-			<div className="grid w-full grid-cols-2 sm:grid-cols-3">
+			<div className="col-span-4 grid w-full grid-cols-2 sm:grid-cols-3">
 				<div className="col-span-1 flex-col space-y-1 py-4 sm:col-span-2">
 					<ul className="flex flex-wrap gap-x-2">
 						{conference.tags.map((tag) => {
@@ -30,9 +33,19 @@ export const ConfListItem = ({ conference }: { conference: Conference }) => {
 							target="_blank"
 							className="cursor-pointer underline-offset-2 hover:text-gray-600 hover:underline"
 						>
-							<p className="text-base font-semibold leading-6 text-gray-900">
+							<p className="flex flex-col gap-x-3 text-base font-semibold leading-6 text-gray-900 sm:flex-row sm:items-center">
 								<span className="absolute inset-x-0 -top-px bottom-0" />
 								{conference.name}
+								{isCFPOpen && (
+									<span className="my-2 -ml-1 inline max-w-max rounded bg-gray-100 px-1.5 py-0.5 text-xs font-semibold tabular-nums text-gray-700">
+										CFP open until{" "}
+										{new Date(conference.cfp?.close ?? "").toLocaleDateString("en-US", {
+											month: "short",
+											day: "numeric",
+											year: "numeric",
+										})}
+									</span>
+								)}
 							</p>
 						</Link>
 						<p className="text-xs text-gray-900">{conference.location}</p>
