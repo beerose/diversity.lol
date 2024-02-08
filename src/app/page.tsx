5fb9a3f5c2331d0d256267c44c557acaaa163f80
beerose <alexsandra.sikora@gmail.com>
2024-02-08 15:45:42 +0100

    Placeholder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,20 @@
-import { ConfList } from "@/ui/ConfList";
-import { Cta } from "@/ui/Cta";
-import { Footer } from "@/ui/Footer";
-import { Hero } from "@/ui/Hero";
-import { getAllTags, getConferencesFor } from "@/utils/data";
-import { sortValues, thresholdValues } from "@/utils/filters";
+import { Hero } from "@/ui/HeroPlaceholder";
 
-export default async function Home({
-	searchParams,
-}: {
-	searchParams: Record<string, string | string[] | undefined>;
-}) {
-	const sort = sortValues.includes(searchParams.sort) ? searchParams.sort : undefined;
-	const location = [searchParams.location].flat().filter(Boolean);
-	const threshold = thresholdValues?.includes(searchParams.threshold) ? searchParams.threshold : undefined;
-	const searchTags = [searchParams.tags].flat().filter(Boolean);
+export default async function Home() {
+	// const sort = sortValues.includes(searchParams.sort) ? searchParams.sort : undefined;
+	// const location = [searchParams.location].flat().filter(Boolean);
+	// const threshold = thresholdValues?.includes(searchParams.threshold) ? searchParams.threshold : undefined;
+	// const searchTags = [searchParams.tags].flat().filter(Boolean);
 
-	const conferences = await getConferencesFor({ filters: { location, threshold, tags: searchTags }, sort });
-	const tags = await getAllTags();
+	// const conferences = await getConferencesFor({ filters: { location, threshold, tags: searchTags }, sort });
+	// const tags = await getAllTags();
 
 	return (
 		<>
 			<Hero />
-			<ConfList tags={tags} conferences={conferences} />
-			<Cta />
-			<Footer />
+			{/* <ConfList tags={tags} conferences={conferences} /> */}
+			{/* <Cta /> */}
+			{/* <Footer /> */}
 		</>
 	);
 }
